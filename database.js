2c0bc1ea5f410e0e95de6a7306c7fff76471cee2
Jyeung29 <147353015+Jyeung29@users.noreply.github.com>
2025-01-26 16:59:33 -0800

    Add files via upload

--- database.js
+++ database.js
@@ -1,11 +1,11 @@
 var mysql = require('mysql');
 
 var conn=mysql.createConnection({
-    host:
-    user:
-    password:
-    database:
-    port:
+    host:"475spongebob.mysql.database.azure.com", 
+    user:"spongebobAdmins", 
+    password:"@css475UWBfa11", 
+    database:"streamingservicetvshow", 
+    port:3306
 });
 
 conn.connect(function(err) {
